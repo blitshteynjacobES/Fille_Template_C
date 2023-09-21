30e57da625cc38735764f87f701252faea7e8ab0
Blitshteyn Jacob <113518112+blitshteynjacobES@users.noreply.github.com>
2023-09-21 08:48:54 +0200

    Add files via upload

---
+++ templates/templates/Template_fr.c
@@ -0,0 +1,31 @@
+/*
+*------------------------------------------------------------------------
+* Nom du projet					: 
+* Date de création du projet	: 
+*------------------------------------------------------------------------
+* Nom de-s l'auteur-s			: 
+* Date de création du fichier	: 
+*------------------------------------------------------------------------
+* Nom du dernier éditeur		: 
+* Version						: x.x
+* Date de dernière modification	: 
+*------------------------------------------------------------------------
+* But du fichiers				: 
+* commentaire					: 
+*------------------------------------------------------------------------
+*/
+
+/*-- librairie publique --*/
+
+
+/*-- librairie privé --*/
+
+
+/*-- déclaration variable --*/
+
+
+/*-- fontion princpale --*/
+
+
+/*-- documentation --*/
+
